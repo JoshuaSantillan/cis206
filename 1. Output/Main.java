7a6660995d6aca31b48f1c639ac1004ce98ed6ad
Joshua <37349382+JoshuaSantillan@users.noreply.github.com>
2020-07-14 01:21:03 -0700

    Add files via upload

---
+++ 1. Output/Main.java
@@ -0,0 +1,36 @@
+/*
+File: Main.java
+Description: First Program
+Author: Joshua Santillan
+Email: santillanj125821@student.vvc.edu
+Date of Creation: 2/16/2019
+*/
+class Main {
+
+  public static void main(String[] args) {
+      //using the join method to auto concatenate lines
+      String house = String.join("\n"
+      ,"   +"
+      ," +   +"
+      ,"+     +"
+      ,"+-----+"
+      ,"| .-. |"
+      ,"| | | |"
+      ,"+-+-+-+");
+
+      System.out.println(house);
+
+    }
+  }
+
+
+
+/*
+   +
+ +   +
++     +
++-----+
+| .-. |
+| | | |
++-+-+-+
+*/
---
+++ 1. Output/README.md
@@ -0,0 +1,2 @@
+# cis206-project-1-JoshuaSantillan
+cis206-project-1-JoshuaSantillan created by GitHub Classroom
